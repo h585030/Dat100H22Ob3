33931cbdf0943269facafeee252011b1ea1cc785
h585030 <70582971+h585030@users.noreply.github.com>
2022-11-17 02:15:58 +0100

    Add files via upload

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,15 +1,28 @@
 package no.hvl.dat100.jplab11.oppgave4;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
-import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		File file = new File(mappe + filnavn);
+		FileWriter writer;
+		boolean bool = false;
+		
+		try {
+			writer = new FileWriter(file);
+			writer.write(samling.toString());
+			writer.close();
+			bool = true;
+		} catch (IOException e) {
+			System.out.println(e);
+		} finally {
+			return bool;
+		}
 	}
 }
